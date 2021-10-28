53acc7d9d8fa97b5ba816776cbcaa1317bb679ef
aliceson89 <alice.son1109@gmail.com>
2021-10-28 17:53:52 -0400

    edit aws-region with static value

--- scripts/beforeInstall.bash
+++ scripts/beforeInstall.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#step 1 : backup existing kcsaweb
-if [ -d /home/ubuntu/kcsaweb ]; then
-    sudo cp -r  /home/ubuntu/kcsaweb/* /home/ubuntu/bkup/kcsa_$(date '+%Y_%m_%d')/
-    echo 'Here is the current BKUP DIR List (/home/ubuntu/bkup/): '
-    ls -ltr /home/ubuntu/bkup/kcsa_$(date '+%Y_%m_%d')
-fi
+# #step 1 : backup existing kcsaweb
+# if [ -d /home/ubuntu/kcsaweb ]; then
+#     sudo cp -r  /home/ubuntu/kcsaweb/* /home/ubuntu/bkup/kcsa_$(date '+%Y_%m_%d')/
+#     echo 'Here is the current BKUP DIR List (/home/ubuntu/bkup/): '
+#     ls -ltr /home/ubuntu/bkup/kcsa_$(date '+%Y_%m_%d')
+# fi
 
 #step 2 : delete existing kcsaweb
 if [ -d /home/ubuntu/kcsaweb ]; then
