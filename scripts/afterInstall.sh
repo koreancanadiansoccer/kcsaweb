8486a3ed81bcf5f0d2cecc734c4b1da2fa7cd775
aliceson89 <alice.son1109@gmail.com>
2021-10-28 23:12:15 -0400

    edit appspec.yml

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -10,13 +10,14 @@ sudo npm install
 #copy .env file from /home/ubuntu/config to Root DIR for kcsaweb
 sudo cp /home/ubuntu/config/.env /home/ubuntu/kcsaweb/.
 #migrate sequelize
-sudo npx sequelize-cli seed:generate --name [name of seed file]
-sudo npx sequelize-cli db:seed:all
-sudo npx sequelize-cli migration:generate --name [name_of_your_migration]
 sudo npx sequelize-cli db:migrate
+# sudo npx sequelize-cli seed:generate --name [name of seed file]
+# sudo npx sequelize-cli db:seed:all
+# sudo npx sequelize-cli migration:generate --name [name_of_your_migration]
+# sudo npx sequelize-cli db:migrate
 
 #Set up nginx
-sudo cp -r /home/ubuntu/kcsa/client/build/* /var/www/kcsa-demo/
+sudo cp -r /home/ubuntu/kcsaweb/client/build* /var/www/kcsa-demo/
 
 
 
