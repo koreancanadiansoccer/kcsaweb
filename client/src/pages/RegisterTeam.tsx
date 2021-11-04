094c78492f3019dc370c7e7662af3aec0a7b74e9
dhmoon91 <dhmoon91@gmail.com>
2021-11-04 05:51:06 -0400

    animate

--- client/src/pages/RegisterTeam.tsx
+++ client/src/pages/RegisterTeam.tsx
@@ -1,8 +1,8 @@
 import React, {
   FunctionComponent,
   useEffect,
-  useCallback,
   useContext,
+  useState,
 } from 'react';
 import Box from '@material-ui/core/Box';
 import Stepper from '@material-ui/core/Stepper';
@@ -10,6 +10,8 @@ import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
 import Container from '@material-ui/core/Container';
 import { useHistory } from 'react-router-dom';
+import { motion, AnimatePresence } from 'framer-motion';
+import { map } from 'lodash';
 
 import { ACCOUNTSTATUS } from '../types/user';
 import { TeamConfig } from '../components/register_team/TeamConfig';
@@ -19,18 +21,24 @@ import { ViewerContext } from '../context/homeViewer';
 export const RegisterTeam: FunctionComponent = () => {
   const { viewer } = useContext(ViewerContext);
   const history = useHistory();
-
-  //Step configurations
-  const [activeStep, setActiveStep] = React.useState(0);
-  const steps = ['Confugre Team', 'Add Players'];
+  const [direction, setDirection] = useState('right');
 
   const handleNext = () => {
+    setDirection('right');
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
   };
 
   const handleBack = () => {
+    setDirection('left');
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
+
+  //Step configurations
+  const [activeStep, setActiveStep] = React.useState(0);
+  const steps = [
+    { label: 'Confugre Team', comp: <TeamConfig handleNext={handleNext} /> },
+    { label: 'Add Players', comp: <PlayerConfig handleBack={handleBack} /> },
+  ];
 
   useEffect(() => {
     if (!viewer.user || !viewer.user.id) {
@@ -43,28 +51,33 @@ export const RegisterTeam: FunctionComponent = () => {
     }
   }, [viewer]);
 
-  const getStepContent = useCallback((step) => {
-    switch (step) {
-      case 0:
-        return <TeamConfig handleNext={handleNext} />;
-      case 1:
-        return <PlayerConfig handleBack={handleBack} />;
-      default:
-        return 'Unknown step';
-    }
-  }, []);
+  const variants = {
+    initial:
+      direction === 'right'
+        ? { opacity: 0, x: 500, y: 0 }
+        : { opacity: 0, x: -500, y: 0 },
+    animate:
+      direction === 'right'
+        ? { opacity: 1, x: 0, y: 0 }
+        : { opacity: 1, x: 0, y: 0 },
+    exit: { opacity: 0 },
+    // exit:
+    //   direction === 'right'
+    //     ? { opacity: 0, x: -500, y: 0 }
+    //     : { opacity: 0, x: 1000, y: 0 },
+  };
 
   return (
     <Box mt={20} mb={20}>
       <Container>
         <Stepper activeStep={activeStep}>
-          {steps.map((label, idx) => {
+          {steps.map((step, idx) => {
             const stepProps: { completed?: boolean } = {};
             const labelProps: { optional?: React.ReactNode } = {};
 
             return (
-              <Step key={`${label}-${idx}`} {...stepProps}>
-                <StepLabel {...labelProps}>{label}</StepLabel>
+              <Step key={`${step.label}-${idx}`} {...stepProps}>
+                <StepLabel {...labelProps}>{step.label}</StepLabel>
               </Step>
             );
           })}
@@ -75,7 +88,26 @@ export const RegisterTeam: FunctionComponent = () => {
             <Box>All completed</Box>
           ) : (
             <Box>
-              <Container>{getStepContent(activeStep)}</Container>
+              <Container>
+                <AnimatePresence exitBeforeEnter>
+                  {map(
+                    steps,
+                    (step, idx) =>
+                      activeStep === idx && (
+                        <motion.div
+                          key={`step-content-${idx}`}
+                          variants={variants}
+                          initial="initial"
+                          exit="exit"
+                          animate={activeStep === idx ? 'animate' : 'initial'}
+                          transition={{ delay: 0.3 }}
+                        >
+                          {step.comp}
+                        </motion.div>
+                      )
+                  )}
+                </AnimatePresence>
+              </Container>
             </Box>
           )}
         </Box>
